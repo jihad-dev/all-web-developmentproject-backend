80bfbc710090828110f8454968aa6abeb704d6e8
jihad-dev <mdjihadislam166@gmail.com>
2025-04-09 22:09:54 +0600

    done

--- src/server.ts
+++ src/server.ts
@@ -5,6 +5,7 @@ const port = config.port || 5000;
 
 async function main() {
     try {
+    
         await mongoose.connect(config.database_url as string);
         app.listen(port, () => {
             console.log(`Example app listening on port ${port}`)
